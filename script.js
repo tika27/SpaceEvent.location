3120fcd6cab6c32476208901273a0551de805efe
Charles Hall <chall8001@gmail.com>
2021-02-04 14:32:29 -0700

    linked the js files

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-var nasaURL =
+console.log("test")
+// var nasaURL =
 
 
     //Primary Functions
@@ -18,23 +19,23 @@ var nasaURL =
 
 
     //function to find iss. not functioning yet
-    function findISS() {
-        //satellite endpoint
-        var queryURL = "https://api.wheretheiss.at/v1/satellites";
-        //returns position, velocity, and other related info about satellite at any given point in time
-        var location = "https://api.wheretheiss.at/v1/satellites/25544"
-        //returns list of positions about satellite w/ timestamps (up to 10)
-        var futureLocations = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=1436029892,1436029902&units=miles"
-        //returns TLE data--predicts location of satellite for given point in time
-        var TLEData = "https://api.wheretheiss.at/v1/satellites/25544/tles"
-        //returns coordinates 
-        var coord = "https://api.wheretheiss.at/v1/coordinates"
+    // function findISS() {
+    //     //satellite endpoint
+    //     var queryURL = "https://api.wheretheiss.at/v1/satellites";
+    //     //returns position, velocity, and other related info about satellite at any given point in time
+    //     var location = "https://api.wheretheiss.at/v1/satellites/25544"
+    //     //returns list of positions about satellite w/ timestamps (up to 10)
+    //     var futureLocations = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=1436029892,1436029902&units=miles"
+    //     //returns TLE data--predicts location of satellite for given point in time
+    //     var TLEData = "https://api.wheretheiss.at/v1/satellites/25544/tles"
+    //     //returns coordinates 
+    //     var coord = "https://api.wheretheiss.at/v1/coordinates"
 
         
 
       
 
-    }
+    // }
 
 //Primary Functions
 //User wants to see where the space station currently is, and the date it will pass over their region in North America  
